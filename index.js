2e539037c80c1dab77816845e8c07ebdb992fbb5
denis.savgir@codeimmersives.com <denis.savgir@codeimmersives.com>
2020-06-06 13:32:47 -0400

    Working

--- index.js
+++ index.js
@@ -50,3 +50,51 @@
 // Here is a dumb quote site if you want to use. Maybe pick the short ones so it doesn't clutter your console
 // https://wpart.org/20-funny-web-developer-software-programmer-quotes/
 //http://www.devtopics.com/101-great-computer-programming-quotes/
+
+
+const express = require('express');
+const app = express();
+const path = require('path');
+const morgan = require('morgan');
+require('dotenv').config();
+
+const gameRouter = require('./routes/gameRoutes.js');
+const quote = require("./middleware/quotes");
+
+//const logger = require('./middlewares/logger');
+//const timer = require('./middlewares/timer');
+const port = process.env.PORT || 3000;
+
+//app.use(logger);
+app.use(quote);
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/api/v1/games', gameRouter); // Parent route
+
+
+// this hooks up use of static routes
+// app.use(express.static(path.join(__dirname, 'public')))
+// sends string to browser
+// app.get('/', (req, res) => {
+//   res.send('Hello Express App')
+// })
+
+// app.get('/api/v1', (req, res) => {
+//   return res.status(200).json({ confirmation: 'success', users });
+// });
+
+// app.get('/api/v1/:id', (req, res) => {
+//   const user = users.filter(user => user.id === req.params.id);
+
+//   if(user.length == 0) {
+//     return res.status(404).json({confirmation: 'failed', message: "User not found"});
+//   }
+
+//   return res.status(200).json({confirmation: 'success', user});
+//   //res.send(req.params.id);
+// });
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
